c792776726cdb9be93a8c93fe9c422cc7636bc7e
VladErmolenko <vladislav.culiev@gmail.com>
2020-04-14 21:56:40 +0400

    add all tasks

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -4,6 +4,47 @@ class TimeConverter {
 
     // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        var result=" "
+        val nums = arrayOf(
+            "",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen",
+            "twenty",
+            "twenty one",
+            "twenty two",
+            "twenty three",
+            "twenty four",
+            "twenty five",
+            "twenty six",
+            "twenty seven",
+            "twenty eight",
+            "twenty nine", "half")
+        when(minute.toInt()){
+            0-> result="${nums[hour.toInt()]} o' clock"
+            15->result="quarter past ${nums[hour.toInt()]}"
+            in 1..29-> result="${nums[minute.toInt()]} minutes past ${nums[hour.toInt()]}"
+            30-> result="${nums[minute.toInt()]} past ${nums[hour.toInt()]}"
+            45->result="quarter to ${nums[hour.toInt()+1]}"
+            in 31..59-> result="${nums[60-minute.toInt()]} minutes to ${nums[hour.toInt()+1]}"
+            else-> result=""
+        }
+        return result
     }
 }
--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -2,8 +2,80 @@ package subtask4
 
 class Pangram {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        if (inputString.isBlank()) return ""
+
+        val alphabet = "abcdefghijklmnopqrstuvwxyz"
+        val vowels = "AaEeIiOoUuYy"
+        val consonants = "BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvWwXxZz"
+        val sb = StringBuilder()
+        val resultString = StringBuilder()
+        val regex = """(\s)+""".toRegex()
+        val inputStr = regex.replace(inputString.trim(), " ")
+        val strings = inputStr.split(' ')
+        var stringsWithInt = mutableListOf<Pair<String, Int>>()
+        var pangram = true
+        var letterCounter = 0
+
+        for (i in alphabet)
+            if (!inputString.toLowerCase().contains(i))
+                pangram = false
+
+        if (pangram) {
+
+            for (i in strings) {
+                for (j in i) {
+                    if (vowels.contains(j)) {
+                        letterCounter++
+                    }
+                }
+                stringsWithInt.add(i to letterCounter)
+                letterCounter = 0
+            }
+
+            stringsWithInt = stringsWithInt.sortedBy { (_, number) -> number }.toMutableList()
+
+            for (i in stringsWithInt) {
+                sb.append(i.second)
+                for (j in i.first)
+                    if (vowels.contains(j))
+                        sb.append(j.toUpperCase())
+                    else sb.append(j)
+
+                resultString.append(sb)
+                resultString.append(" ")
+                sb.clear()
+            }
+            return resultString.trim().toString()
+
+        } else {
+
+            for (i in strings) {
+                for (j in i) {
+                    if (consonants.contains(j)) {
+                        letterCounter++
+                    }
+                }
+                stringsWithInt.add(i to letterCounter)
+                letterCounter = 0
+            }
+
+            stringsWithInt = stringsWithInt.sortedBy { (_, number) -> number }.toMutableList()
+
+            for (i in stringsWithInt) {
+                sb.append(i.second)
+                for (j in i.first)
+                    if (consonants.contains(j))
+                        sb.append(j.toUpperCase())
+                    else sb.append(j)
+
+                resultString.append(sb)
+                resultString.append(" ")
+                sb.clear()
+            }
+            return resultString.trim().toString()
+        }
+
     }
+
 }
